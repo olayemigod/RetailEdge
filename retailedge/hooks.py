eb24d1cd9a9d8f3a06f15245e3252457e5f6daee
olayemigod <olayemigod@gmail.com>
2026-07-23 12:49:16 +0100

    refactor(retailedge): preserve existing hooks structure

--- retailedge/hooks.py
+++ retailedge/hooks.py
@@ -7,10 +7,14 @@ app_license = "MIT"
 app_logo_url = "/assets/retailedge/logo.png"
 app_home = "/app/retailedge-home"
 
+# Apps
+# ------------------
+
 # ERPNext remains the operational and accounting system of record. EdgeSuite UI
 # supplies the local product-shell and document-workspace runtime.
 required_apps = ["erpnext", "edgesuite_ui"]
 
+# Each item in the list will be shown as an app in the apps page
 add_to_apps_screen = [
 	{
 		"name": app_name,
@@ -21,6 +25,10 @@ add_to_apps_screen = [
 	}
 ]
 
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
 app_include_css = [
 	"/assets/retailedge/css/retailedge_cards.css",
 ]
@@ -30,6 +38,21 @@ app_include_js = [
 	"/assets/retailedge/js/retailedge_product_menu.js",
 ]
 
+# include js, css files in header of web template
+# web_include_css = "/assets/retailedge/css/retailedge.css"
+# web_include_js = "/assets/retailedge/js/retailedge.js"
+
+# include custom scss in every website theme (without file extension ".scss")
+# website_theme_scss = "retailedge/public/scss/website"
+
+# include js in web form
+# webform_include_js = {"doctype": "public/js/doctype.js"}
+# webform_include_css = {"doctype": "public/css/doctype.css"}
+
+# include js in page
+# Report summary cards are styled via native Frappe DOM selectors in CSS.
+
+# include js in doctype views
 doctype_js = {
 	"Item": "public/js/inventory_documents.js",
 	"Material Request": "public/js/material_request.js",
@@ -59,15 +82,95 @@ doctype_js = {
 	"Packed Item": "public/js/cost_visibility_child_table.js",
 	"Item Default": "public/js/inventory_documents.js",
 }
-
 doctype_list_js = {
 	"Purchase Receipt": "public/js/purchase_documents_list.js",
 	"Purchase Invoice": "public/js/purchase_documents_list.js",
-	"Purchase Order": "public/js/purchase_documents_list.js",
+	"Purchase Order": "public/js/purchase_order.js",
 	"RetailEdge Cashier Expense": "public/js/retailedge_cashier_expense_list.js",
 	"RetailEdge Payment Statement Import": "public/js/payment_statement_import_list.js",
 }
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
+# Svg Icons
+# ------------------
+# include app icons in desk
+# app_include_icons = "retailedge/public/icons.svg"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+# 	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# automatically load and sync documents of this doctype from downstream apps
+# importable_doctypes = [doctype_1]
+
+# Jinja
+# ----------
+
+# add methods and filters to jinja environment
+# jinja = {
+# 	"methods": "retailedge.utils.jinja_methods",
+# 	"filters": "retailedge.utils.jinja_filters"
+# }
+
+# Installation
+# ------------
+
+# before_install = "retailedge.install.before_install"
+# after_install = "retailedge.install.after_install"
+
+# Uninstallation
+# ------------
+
+# before_uninstall = "retailedge.uninstall.before_uninstall"
+# after_uninstall = "retailedge.uninstall.after_uninstall"
+
+# Integration Setup
+# ------------------
+# To set up dependencies/integrations with other apps
+# Name of app being installed is passed as an argument
+
+# before_app_install = "retailedge.utils.before_app_install"
+# after_app_install = "retailedge.utils.after_app_install"
+
+# Integration Cleanup
+# -------------------
+# To clean up dependencies/integrations with other apps
+# Name of app being uninstalled is passed as an argument
+
+# before_app_uninstall = "retailedge.utils.before_app_uninstall"
+# after_app_uninstall = "retailedge.utils.after_app_uninstall"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "retailedge.notifications.get_notification_config"
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
+# }
 doc_events = {
 	"Sales Invoice": {
 		"validate": "retailedge.branch_defaults_application.apply_branch_attribution_and_defaults",
@@ -121,11 +224,119 @@ doc_events = {
 	},
 }
 
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"retailedge.tasks.all"
+# 	],
+# 	"daily": [
+# 		"retailedge.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"retailedge.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"retailedge.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"retailedge.tasks.monthly"
+# 	],
+# }
+
+# Testing
+# -------
+
+# before_tests = "retailedge.install.before_tests"
 after_migrate = [
 	"retailedge.setup_roles.ensure_retailedge_roles",
 	"retailedge.transaction_branch_attribution.ensure_transaction_branch_custom_fields",
 	"retailedge.sales_invoice_verification_sync.ensure_sales_invoice_verification_custom_fields",
 	"retailedge.workspace_sync.sync_retailedge_workspace_layout",
 ]
-
 boot_session = "retailedge.boot.boot_session"
+
+# Extend DocType Class
+# ------------------------------
+#
+# Specify custom mixins to extend standard doctype controller.
+# extend_doctype_class = {
+# 	"Task": "retailedge.custom.task.CustomTaskMixin"
+# }
+
+# Overriding Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "retailedge.event.get_events"
+# }
+#
+# each overriding function accepts a `data` argument;
+# generated from base implementation of doctype dashboard,
+# along with any modifications made in other Frappe apps
+# override_doctype_dashboards = {
+# 	"Task": "retailedge.task.get_dashboard_data"
+# }
+
+# exempt linked doctypes from being automatically cancelled
+#
+# auto_cancel_exempted_doctypes = ["Auto Repeat"]
+
+# Ignore links to specified DocTypes when deleting documents
+# -----------------------------------------------------------
+
+# ignore_links_on_delete = ["Communication", "ToDo"]
+
+# Request Events
+# ----------------
+# before_request = ["retailedge.utils.before_request"]
+# after_request = ["retailedge.utils.after_request"]
+
+# Job Events
+# ----------
+# before_job = ["retailedge.utils.before_job"]
+# after_job = ["retailedge.utils.after_job"]
+
+# User Data Protection
+# --------------------
+
+# user_data_fields = [
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
+# ]
+
+# Authentication and authorization
+# --------------------------------
+
+# auth_hooks = [
+# 	"retailedge.auth.validate"
+# ]
+
+# Automatically update python controller files with type annotations for this app.
+# export_python_type_annotations = True
+
+# default_log_clearing_doctypes = {
+# 	"Logging DocType Name": 30  # days to retain logs
+# }
+
+# Translation
+# ------------
+# List of apps whose translatable strings should be excluded from this app's translations.
+# ignore_translatable_strings_from = []
