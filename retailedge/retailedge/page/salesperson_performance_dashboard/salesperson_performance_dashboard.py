1aab522ac1d5979e297676ba99cb6c15dbccd287
olayemigod <olayemigod@gmail.com>
2026-08-11 17:50:06 +0100

    reconcile(retailedge): remove isolated dashboard page provider

--- retailedge/retailedge/page/salesperson_performance_dashboard/salesperson_performance_dashboard.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, ProcessEdge Solutions and contributors
-# For license information, please see license.txt
-
-import frappe
-
-
-def get_context(context):
-	"""Minimal page context provider."""
-	return {"title": "Salesperson Performance Dashboard"}
