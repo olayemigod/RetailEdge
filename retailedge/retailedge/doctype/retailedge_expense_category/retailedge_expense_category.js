d5307a07ab032430451e2959883ca5e0ad624312
olayemigod <olayemigod@gmail.com>
2026-08-17 08:01:49 +0100

    feat(expenses): cascade expense category accounting fields

--- retailedge/retailedge/doctype/retailedge_expense_category/retailedge_expense_category.js
+++ retailedge/retailedge/doctype/retailedge_expense_category/retailedge_expense_category.js
@@ -4,6 +4,9 @@ frappe.ui.form.on("RetailEdge Expense Category", {
 			return {
 				filters: {
 					company: frm.doc.company || "",
+					root_type: "Expense",
+					is_group: 0,
+					disabled: 0,
 				},
 			};
 		});
@@ -12,8 +15,24 @@ frappe.ui.form.on("RetailEdge Expense Category", {
 			return {
 				filters: {
 					company: frm.doc.company || "",
+					is_group: 0,
 				},
 			};
 		});
+	},
+
+	company(frm) {
+		if (frm.__retailedge_expense_category_company === undefined) {
+			frm.__retailedge_expense_category_company = frm.doc.company || "";
+			return;
+		}
+		const previousCompany = frm.__retailedge_expense_category_company;
+		const nextCompany = frm.doc.company || "";
+		frm.__retailedge_expense_category_company = nextCompany;
+		if (previousCompany === nextCompany) {
+			return;
+		}
+		frm.set_value("expense_account", null);
+		frm.set_value("default_cost_center", null);
 	},
 });
