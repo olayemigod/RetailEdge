fc48ed2e28946c72df2ed489ed9642d902d717e1
olayemigod <olayemigod@gmail.com>
2026-08-19 10:49:42 +0100

    feat: load refined bank match review UI

--- retailedge/retailedge/page/bank_matching_reconciliation/bank_matching_reconciliation.js
+++ retailedge/retailedge/page/bank_matching_reconciliation/bank_matching_reconciliation.js
@@ -3,6 +3,7 @@
 
 	const PAGE_NAME = "bank-matching-reconciliation";
 	const ASSET = "/assets/retailedge/js/bank_matching_reconciliation.js";
+	const REVIEW_ASSET = "/assets/retailedge/js/bank_match_review_ui.js";
 
 	function startWorkspace(wrapper) {
 		if (typeof window.retailedgeBootBankingWorkspace !== "function") {
@@ -12,12 +13,8 @@
 	}
 
 	function boot(wrapper) {
-		if (typeof window.retailedgeBootBankingWorkspace === "function") {
-			startWorkspace(wrapper);
-			return;
-		}
-
 		Promise.resolve(frappe.require(ASSET))
+			.then(() => Promise.resolve(frappe.require(REVIEW_ASSET)))
 			.then(() => startWorkspace(wrapper))
 			.catch((error) => {
 				console.error("RetailEdge Banking workspace asset failed to load", error);
