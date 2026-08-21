4d1ff7a33bf3ff82e4f346bd112b6985be502b5b
olayemigod <olayemigod@gmail.com>
2026-08-21 20:30:23 +0100

    fix(retailedge): guard direct follow-up edits

--- retailedge/retailedge/doctype/retailedge_action_follow_up/retailedge_action_follow_up.py
+++ retailedge/retailedge/doctype/retailedge_action_follow_up/retailedge_action_follow_up.py
@@ -8,6 +8,7 @@ from frappe.utils import get_datetime
 
 class RetailEdgeActionFollowUp(Document):
 	def validate(self):
+		self._validate_controlled_write()
 		if self.assigned_to:
 			enabled = frappe.db.get_value("User", self.assigned_to, "enabled")
 			if not enabled:
@@ -17,3 +18,14 @@ class RetailEdgeActionFollowUp(Document):
 		if self.follow_up_on and self.status == "Snoozed" and self.snoozed_until:
 			if get_datetime(self.follow_up_on) < get_datetime(self.snoozed_until):
 				frappe.throw(_("Follow Up On cannot be before Snoozed Until."))
+
+	def _validate_controlled_write(self):
+		user = frappe.session.user
+		if user == "Administrator" or "System Manager" in set(frappe.get_roles(user)):
+			return
+		if getattr(frappe.flags, "retailedge_action_follow_up_api_write", False):
+			return
+		frappe.throw(
+			_("Action Centre follow-up state can only be changed from the Action Centre."),
+			frappe.PermissionError,
+		)
