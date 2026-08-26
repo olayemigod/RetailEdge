3342b86cb0977fb152146ca09adb25ffb151e7d1
olayemigod <olayemigod@gmail.com>
2026-08-26 16:23:56 +0100

    feat: load fuzzy discovery and dense banking UI

--- retailedge/retailedge/page/bank_matching_reconciliation/bank_matching_reconciliation.js
+++ retailedge/retailedge/page/bank_matching_reconciliation/bank_matching_reconciliation.js
@@ -4,6 +4,8 @@
 	const PAGE_NAME = "bank-matching-reconciliation";
 	const OUTCOME_ASSET = "/assets/retailedge/js/bank_candidate_outcome_notifications.js";
 	const REVIEW_CATEGORY_ASSET = "/assets/retailedge/js/bank_review_category_adapter.js";
+	const FUZZY_DISCOVERY_ASSET = "/assets/retailedge/js/bank_matching_fuzzy_discovery_adapter.js";
+	const DENSE_WORKSPACE_CSS = "/assets/retailedge/css/bank_matching_dense_workspace.css";
 	const WORKSPACE_ASSET = "/assets/retailedge/js/bank_matching_edgesuite_workspace.js";
 
 	function startWorkspace(wrapper) {
@@ -16,6 +18,8 @@
 	function boot(wrapper) {
 		Promise.resolve(frappe.require(OUTCOME_ASSET))
 			.then(() => Promise.resolve(frappe.require(REVIEW_CATEGORY_ASSET)))
+			.then(() => Promise.resolve(frappe.require(FUZZY_DISCOVERY_ASSET)))
+			.then(() => Promise.resolve(frappe.require(DENSE_WORKSPACE_CSS)))
 			.then(() => Promise.resolve(frappe.require(WORKSPACE_ASSET)))
 			.then(() => startWorkspace(wrapper))
 			.catch((error) => {
