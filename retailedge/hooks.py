64a7db610a9612da136af67aa580a6bfbf4fa99a
olayemigod <olayemigod@gmail.com>
2026-08-26 12:40:06 +0100

    fix: defer business hub controller until Desk runtime

--- retailedge/hooks.py
+++ retailedge/hooks.py
@@ -35,7 +35,7 @@ app_include_css = [
 ]
 app_include_js = [
 	"/assets/retailedge/js/retailedge.js",
-	"/assets/retailedge/js/retailedge_business_hub_page.js",
+	"/assets/retailedge/js/retailedge_business_hub_bootstrap.js",
 	"/assets/retailedge/js/retailedge_reporting_actions.js",
 	"/assets/retailedge/js/new_document_operating_defaults.js",
 ]
@@ -123,8 +123,7 @@ doctype_list_js = {
 # importable_doctypes = [doctype_1]
 
 # Jinja
-# ----------
-
+# ------------------
 # add methods and filters to jinja environment
 # jinja = {
 # 	"methods": "retailedge.utils.jinja_methods",
@@ -152,8 +151,8 @@ doctype_list_js = {
 
 # Integration Cleanup
 # -------------------
-# before_app_uninstall = "retailedge.utils.before_app_uninstall"
-# after_app_uninstall = "retailedge.utils.after_app_uninstall"
+# before_app_uninstall = "retailedge.utils.before_uninstall"
+# after_app_uninstall = "retailedge.utils.after_uninstall"
 
 # Desk Notifications
 # ------------------
