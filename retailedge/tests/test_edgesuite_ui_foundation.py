a74e1cf05528499d4388eeabe620334be4281af0
olayemigod <olayemigod@gmail.com>
2026-08-31 01:05:27 +0100

    test(e16): update supplier payables navigation contract

--- retailedge/tests/test_edgesuite_ui_foundation.py
+++ retailedge/tests/test_edgesuite_ui_foundation.py
@@ -60,7 +60,7 @@ class RetailEdgeEdgeSuiteUIFoundationTests(unittest.TestCase):
 		)
 		self.assertEqual(
 			{item["target"] for item in groups["suppliers-payables"]["items"]},
-			{"Supplier", "supplier-payables", "Accounts Payable"},
+			{"Supplier", "supplier-payables", "Payment Order", "Accounts Payable"},
 		)
 		self.assertIn(
 			"RetailEdge Stock Movement History",
