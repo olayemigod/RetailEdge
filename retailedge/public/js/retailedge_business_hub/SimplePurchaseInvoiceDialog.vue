da7457f0adb19f987d9893f2b2dee8da3bf8e7b1
olayemigod <olayemigod@gmail.com>
2026-09-11 16:00:10 +0100

    fix(retailedge): invalidate dependent pricing on Branch change

--- retailedge/public/js/retailedge_business_hub/SimplePurchaseInvoiceDialog.vue
+++ retailedge/public/js/retailedge_business_hub/SimplePurchaseInvoiceDialog.vue
@@ -361,6 +361,7 @@ export default {
 			const branch = next || "";
 			this.values.branch = branch;
 			this.values.warehouse = "";
+			this.values.items = (this.values.items || []).map((row) => ({ ...row, rate: "" }));
 			this.pricingCache.clear();
 			if (!branch || !this.values.company) return;
 			const token = ++this.cascadeToken;
