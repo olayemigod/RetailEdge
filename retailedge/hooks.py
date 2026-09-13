2cd062f04914a472585d75dae900d43bbec4a51f
olayemigod <olayemigod@gmail.com>
2026-09-13 20:06:05 +0100

    qa: load RetailEdge identity after existing product styles

--- retailedge/hooks.py
+++ retailedge/hooks.py
@@ -32,6 +32,7 @@ app_include_css = [
 	"/assets/retailedge/css/retailedge_cards.css",
 	"/assets/retailedge/css/retailedge_workspace_home.css",
 	"/assets/retailedge/css/retailedge_guided_create_menu.css",
+	"/assets/retailedge/css/retailedge_product_identity.css",
 ]
 app_include_js = [
 	"/assets/retailedge/js/retailedge.js",
