982db09c40bbfe8e8670dc00945b3ae77c62ffa8
olayemigod <olayemigod@gmail.com>
2026-09-13 23:52:16 +0100

    phase2d: block Sales form-submit context bypass

--- retailedge/public/js/retailedge_business_hub/SimpleSalesInvoiceDialog.vue
+++ retailedge/public/js/retailedge_business_hub/SimpleSalesInvoiceDialog.vue
@@ -496,7 +496,7 @@ export default {
 			});
 		},
 		async saveDraft() {
-			if (this.saving || this.loading) return;
+			if (this.saving || this.loading || !this.transactionContextReady) return;
 			this.saveError = "";
 			this.saving = true;
 			try {
