25175e28d752469bb123e0a68d6b3ebd8bdb55e5
olayemigod <olayemigod@gmail.com>
2026-09-14 11:14:17 +0100

    phase5: preserve Business Hub scope in supplier-payables

--- retailedge/public/js/purchase_reporting/PurchaseReportingReport.vue
+++ retailedge/public/js/purchase_reporting/PurchaseReportingReport.vue
@@ -166,7 +166,11 @@ export default {
 				const navigationPromise = typeof window.retailedgeGetBusinessHubContext === "function" ? window.retailedgeGetBusinessHubContext() : callMethod("retailedge.edgesuite_ui.get_retailedge_business_hub_context");
 				const [context, navigation] = await Promise.all([callMethod("retailedge.purchase_reporting.get_purchase_reporting_context"), navigationPromise]);
 				this.filters = { ...this.filters, ...(context.default_filters || {}) };
-				this.tenantName = context.tenant_name || this.filters.company || ""; this.branchName = context.branch_name || this.filters.branch || ""; this.userName = context.user_name || ""; this.companyCurrency = context.company_currency || "";
+				const hubHandoff = this.reportType === "supplier_payables"
+					? window.retailedgeConsumeBusinessHubRouteOptions?.("supplier-payables") || {}
+					: {};
+				this.filters = { ...this.filters, ...hubHandoff };
+				this.tenantName = hubHandoff.company || context.tenant_name || this.filters.company || ""; this.branchName = hubHandoff.branch || context.branch_name || this.filters.branch || ""; this.userName = context.user_name || ""; this.companyCurrency = context.company_currency || "";
 				this.menuItems = this.mapNavigationGroups(navigation.navigation_groups || []);
 				this.canUseNativeDesk = Boolean(navigation?.access?.can_use_native_desk);
 				if (this.requiredReady) await this.fetchData();
