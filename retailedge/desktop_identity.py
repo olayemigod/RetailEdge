d21d0f0a20829dfe82e7d1991679818186f81f7c
olayemigod <olayemigod@gmail.com>
2026-09-15 23:15:31 +0100

    fix: use blue Shelf R for desktop app icon

--- retailedge/desktop_identity.py
+++ retailedge/desktop_identity.py
@@ -6,7 +6,7 @@ import frappe
 APP_NAME = "retailedge"
 DESKTOP_LABEL = "PEdge Retail"
 DESKTOP_ROUTE = "/desk/retailedge-business-hub"
-DESKTOP_LOGO = "/assets/retailedge/images/processedge_retail/pedge-retail-app-icon-dark-v2.svg"
+DESKTOP_LOGO = "/assets/retailedge/images/processedge_retail/pedge-retail-app-icon-blue-v1.svg"
 LEGACY_LABELS = {"RetailEdge", "ProcessEdge Retail"}
 WORKSPACE_NAME = "RetailEdge"
 
