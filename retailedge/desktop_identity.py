e0f70acc08ed47638b488551f5e9309f399287b3
olayemigod <olayemigod@gmail.com>
2026-09-15 22:44:43 +0100

    fix: point desktop launcher to dark Shelf R vector icon

--- retailedge/desktop_identity.py
+++ retailedge/desktop_identity.py
@@ -6,7 +6,7 @@ import frappe
 APP_NAME = "retailedge"
 DESKTOP_LABEL = "PEdge Retail"
 DESKTOP_ROUTE = "/desk/retailedge-business-hub"
-DESKTOP_LOGO = "/assets/retailedge/images/processedge_retail/pedge-retail-app-icon-dark.png"
+DESKTOP_LOGO = "/assets/retailedge/images/processedge_retail/pedge-retail-app-icon-dark-v2.svg"
 LEGACY_LABELS = {"RetailEdge", "ProcessEdge Retail"}
 WORKSPACE_NAME = "RetailEdge"
 
