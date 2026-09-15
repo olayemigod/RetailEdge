ce26f92b65e8792a7a77551da316528bf795f5e4
olayemigod <olayemigod@gmail.com>
2026-09-15 18:30:34 +0100

    fix(selling): avoid v-if v-for overlap in completion footer

--- retailedge/public/js/professional_selling/StandardSellingCompletionDialog.vue
+++ retailedge/public/js/professional_selling/StandardSellingCompletionDialog.vue
@@ -81,26 +81,27 @@
 				</div>
 				<div class="selling-completion-actions">
 					<button type="button" class="edge-button edge-button--secondary" :disabled="busy" @click="requestClose">Close</button>
-					<button
-						v-if="!completedResult && preview?.can_submit"
-						type="button"
-						class="edge-button edge-button--primary"
-						:disabled="busy"
-						@click="submitDocument"
-					>
-						{{ busy ? "Submitting..." : "Submit" }}
-					</button>
-					<button
-						v-if="!completedResult"
-						v-for="action in workflowActions"
-						:key="action.action"
-						type="button"
-						class="edge-button edge-button--primary"
-						:disabled="busy || !preview?.workflow_eligible"
-						@click="applyWorkflow(action.action)"
-					>
-						{{ action.action }}
-					</button>
+					<template v-if="!completedResult">
+						<button
+							v-if="preview?.can_submit"
+							type="button"
+							class="edge-button edge-button--primary"
+							:disabled="busy"
+							@click="submitDocument"
+						>
+							{{ busy ? "Submitting..." : "Submit" }}
+						</button>
+						<button
+							v-for="action in workflowActions"
+							:key="action.action"
+							type="button"
+							class="edge-button edge-button--primary"
+							:disabled="busy || !preview?.workflow_eligible"
+							@click="applyWorkflow(action.action)"
+						>
+							{{ action.action }}
+						</button>
+					</template>
 				</div>
 			</div>
 		</template>
