2a63f791f329913972e440217d1502927a6d69cc
olayemigod <olayemigod@gmail.com>
2026-09-22 11:06:45 +0100

    feat(r13): add Purchase Analysis capability

--- retailedge/reporting_capabilities.py
+++ retailedge/reporting_capabilities.py
@@ -70,6 +70,12 @@ _REPORT_SPECS = {
 		print_roles=_roles(_MANAGER_ROLES, _BRANCH_MANAGER_ROLES, _SALES_MANAGER_ROLES, _ACCOUNTS_MANAGER_ROLES),
 		export_roles=_roles(_MANAGER_ROLES, _SALES_MANAGER_ROLES, _ACCOUNTS_MANAGER_ROLES), ref_doctype="Sales Invoice",
 	),
+	"purchase-analysis": ReportCapabilitySpec(
+		key="purchase-analysis", label="Purchase Analysis",
+		view_roles=_roles(_MANAGER_ROLES, _BRANCH_MANAGER_ROLES, _PURCHASE_MANAGER_ROLES, _PURCHASE_USER_ROLES, _ACCOUNTS_MANAGER_ROLES, _ACCOUNTS_USER_ROLES),
+		print_roles=_roles(_MANAGER_ROLES, _BRANCH_MANAGER_ROLES, _PURCHASE_MANAGER_ROLES, _ACCOUNTS_MANAGER_ROLES),
+		export_roles=_roles(_MANAGER_ROLES, _PURCHASE_MANAGER_ROLES, _ACCOUNTS_MANAGER_ROLES), ref_doctype="Purchase Invoice",
+	),
 	"purchase-register": ReportCapabilitySpec(
 		key="purchase-register", label="Purchase Register",
 		view_roles=_roles(_MANAGER_ROLES, _BRANCH_MANAGER_ROLES, _PURCHASE_MANAGER_ROLES, _PURCHASE_USER_ROLES, _ACCOUNTS_MANAGER_ROLES, _ACCOUNTS_USER_ROLES),
