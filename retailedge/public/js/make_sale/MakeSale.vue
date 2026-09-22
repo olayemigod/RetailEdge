b6689b97ab8927b63b1058a5727461d1ffce21c4
olayemigod <olayemigod@gmail.com>
2026-09-22 16:22:20 +0100

    fix: scope Sales return handoff to signed-in user

--- retailedge/public/js/make_sale/MakeSale.vue
+++ retailedge/public/js/make_sale/MakeSale.vue
@@ -939,6 +939,7 @@ export default {
 						doctype: "Sales Invoice",
 						name: result.name,
 						source_mode: "sales_return",
+						user: frappe.session?.user || "Guest",
 					};
 					frappe.set_route("professional-selling");
 				} catch (error) {
