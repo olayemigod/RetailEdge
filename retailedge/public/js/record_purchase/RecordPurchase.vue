34d2255f74ed6c0e919611aa5f46846a0cfa7734
olayemigod <olayemigod@gmail.com>
2026-09-22 16:13:12 +0100

    fix: preserve Purchase Invoice item row identity on page edits

--- retailedge/public/js/record_purchase/RecordPurchase.vue
+++ retailedge/public/js/record_purchase/RecordPurchase.vue
@@ -220,6 +220,7 @@ export default {
 							bill_date: this.values.bill_date || "",
 							remarks: this.values.remarks || "",
 							items: (this.values.items || []).filter((row) => row?.item_code).map((row) => ({
+								name: row.name || "",
 								item_code: row.item_code,
 								qty: Number(row.qty || 0),
 								rate: row.rate,
@@ -258,7 +259,7 @@ export default {
 			if (Object.prototype.hasOwnProperty.call(result, "bill_no")) this.values.bill_no = result.bill_no || "";
 			if (Object.prototype.hasOwnProperty.call(result, "bill_date")) this.values.bill_date = result.bill_date || "";
 			if (Object.prototype.hasOwnProperty.call(result, "remarks")) this.values.remarks = result.remarks || "";
-			if (Array.isArray(result.editable_items)) this.values.items = result.editable_items.map((row) => ({ item_code: row.item_code || "", qty: row.qty, rate: row.rate }));
+			if (Array.isArray(result.editable_items)) this.values.items = result.editable_items.map((row) => ({ name: row.name || "", item_code: row.item_code || "", qty: row.qty, rate: row.rate }));
 		},
 		openCompletion() { if (this.savedDocument?.name) { this.editingSavedDraft = false; this.completionOpen = true; } },
 		handleCompletionChanged(result) {
