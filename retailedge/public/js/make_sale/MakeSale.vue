8c170e367daa18a8ce4cc31e95c9f0e20dad0e7d
olayemigod <olayemigod@gmail.com>
2026-09-22 16:12:49 +0100

    fix: preserve Sales Invoice item row identity on page edits

--- retailedge/public/js/make_sale/MakeSale.vue
+++ retailedge/public/js/make_sale/MakeSale.vue
@@ -804,6 +804,7 @@ export default {
 							posting_date: this.values.posting_date,
 							remarks: this.values.remarks || "",
 							items: (this.values.items || []).filter((row) => row?.item_code).map((row) => ({
+								name: row.name || "",
 								item_code: row.item_code,
 								qty: Number(row.qty || 0),
 								rate: row.rate,
@@ -851,6 +852,7 @@ export default {
 			if (Object.prototype.hasOwnProperty.call(result, "remarks")) this.values.remarks = result.remarks || "";
 			if (Array.isArray(result.editable_items)) {
 				this.values.items = result.editable_items.map((row) => ({
+					name: row.name || "",
 					item_code: row.item_code || "",
 					qty: row.qty,
 					rate: row.rate,
