36290fed23d91c70299e794de766c4cc4ceef25b
olayemigod <olayemigod@gmail.com>
2026-09-23 23:13:03 +0100

    fix(reports): refresh cached handoffs on route change

--- retailedge/retailedge/page/branch_performance_dashboard/branch_performance_dashboard.js
+++ retailedge/retailedge/page/branch_performance_dashboard/branch_performance_dashboard.js
@@ -61,6 +61,19 @@ function refreshPendingBusinessHubHandoff(wrapper) {
 	});
 }
 
+
+function bindBusinessHubHandoffRouteRefresh(wrapper) {
+	if (wrapper._retailedgeBusinessHubHandoffRouteRefresh) return;
+	const refresh = () => {
+		const route = frappe.get_route?.();
+		if (!Array.isArray(route) || String(route[0] || "") !== PAGE_ROUTE) return;
+		refreshPendingBusinessHubHandoff(wrapper);
+	};
+	wrapper._retailedgeBusinessHubHandoffRouteRefresh = refresh;
+	document.addEventListener("page-change", refresh);
+	frappe.router?.on?.("change", refresh);
+}
+
 frappe.pages[PAGE_ROUTE].on_page_load = async function (wrapper) {
 	hideNativePageSidebar(wrapper);
 	const bootLoading = document.createElement("div");
@@ -81,6 +94,7 @@ frappe.pages[PAGE_ROUTE].on_page_load = async function (wrapper) {
 		page.body.append(root);
 		wrapper._retailedgeVueApp = await window.mountBranchPerformanceDashboard(root);
 		wrapper._retailedgePageHasShown = true;
+		bindBusinessHubHandoffRouteRefresh(wrapper);
 		wrapper._retailedgeBranchPerformanceMounted = true;
 	} catch (error) {
 		bootLoading.remove();
