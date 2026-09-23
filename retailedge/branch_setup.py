93b15b149f46b342914ed5c9d601cda371b5bf82
olayemigod <olayemigod@gmail.com>
2026-09-23 10:18:29 +0100

    feat: expose branch price list defaults in setup service

--- retailedge/branch_setup.py
+++ retailedge/branch_setup.py
@@ -25,6 +25,8 @@ EDITABLE_FIELDS = (
 	"default_pos_profile",
 	"default_pos_opening_cash_account",
 	"default_cash_mode_of_payment",
+	"default_selling_price_list",
+	"default_buying_price_list",
 	"default_warehouse",
 	"default_source_warehouse",
 	"default_target_warehouse",
@@ -266,6 +268,8 @@ def search_branch_setup_options(
 
 	search_config = {
 		"default_pos_profile": ("POS Profile", {"company": company, "disabled": 0}),
+		"default_selling_price_list": ("Price List", {"enabled": 1, "selling": 1}),
+		"default_buying_price_list": ("Price List", {"enabled": 1, "buying": 1}),
 		"default_warehouse": ("Warehouse", {"company": company, "is_group": 0, "disabled": 0}),
 		"default_source_warehouse": ("Warehouse", {"company": company, "is_group": 0, "disabled": 0}),
 		"default_target_warehouse": ("Warehouse", {"company": company, "is_group": 0, "disabled": 0}),
@@ -282,7 +286,7 @@ def search_branch_setup_options(
 	}
 	if fieldname not in search_config:
 		frappe.throw(_("Unsupported Branch Setup search field: {0}").format(fieldname))
-	if fieldname != "default_cash_mode_of_payment" and not company:
+	if fieldname not in {"default_cash_mode_of_payment", "default_selling_price_list", "default_buying_price_list"} and not company:
 		return []
 	doctype, filters = search_config[fieldname]
 	return search_link(
