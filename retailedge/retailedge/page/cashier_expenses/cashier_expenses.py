941bd32739c4213e7e2fbb1f23c6c4e0a3ee394b
olayemigod <olayemigod@gmail.com>
2026-09-26 13:47:23 +0100

    feat(expenses): add cashier expenses page controller

---
+++ retailedge/retailedge/page/cashier_expenses/cashier_expenses.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+
+# Standard Frappe Page controller. Cashier Expense data is served by the
+# permission-aware Expense Register endpoints in retailedge.expense_register.
